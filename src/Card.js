fd7d6028e37a08b050da6fc9a0967dc35ebe30d8
IvanSobolev <i@bezlikiy.ru>
2025-07-29 20:16:07 +0300

    feat: v0.1.6

--- src/Card.js
+++ src/Card.js
@@ -6,15 +6,23 @@ export class Card {
         this.container = container;
         this.data = data;
         this.elements = {};
+        this.skinViewer = null;
+        this.animationFrameId = null;
+        
+        const rect = this.container.getBoundingClientRect();
+        this.width = rect.width;
+        this.height = rect.height;
+        this.container.style.perspective = '1500px';
+
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
         
         this.init(); 
     }
 
     async init() {
         this.createDOM();
-        
         await this.applyData();
-        
         this.setupSkinViewer();
         this.setupInteractions();
     }
@@ -55,25 +63,32 @@ export class Card {
     }
     
     setupSkinViewer() {
-        const viewer = new skin3d.View({
+        this.skinViewer = new skin3d.View({
             canvas: this.elements.skinCanvas,
-            width: 360,
-            height: 560,
+            width: this.width,
+            height: this.height,
             background: null,
         });
 
-        viewer.controls.enableRotate = false;
-        viewer.controls.enableZoom = false;
+        this.skinViewer.controls.enableRotate = false;
+        this.skinViewer.controls.enableZoom = false;
 
-        viewer.camera.position.set(-25, 35, 50); 
-        viewer.camera.lookAt(0,0,0);
+        const scaleFactor = this.height / 560;
         
+        this.skinViewer.camera.fov = 75 - (this.width / this.height) * 10;
+        this.skinViewer.camera.updateProjectionMatrix();
 
-        viewer.loadSkin(this.data.skinImage)
+        this.skinViewer.camera.position.set(-25 * scaleFactor, 35 * scaleFactor, 50 * scaleFactor);
+        this.skinViewer.camera.lookAt(0,0,0);
+        
+        this.skinViewer.loadSkin(this.data.skinImage)
             .then(() => {
-                const playerObject = viewer.playerObject;
+                const playerObject = this.skinViewer.playerObject;
+                
+                playerObject.scale.set(scaleFactor, scaleFactor, scaleFactor);
 
                 const skin = playerObject.skin;
+                
                 skin.leftArm.rotation.x = -Math.PI / 4;
                 skin.rightArm.rotation.x = Math.PI / 4;
                 skin.leftLeg.rotation.x = Math.PI / 5;
@@ -81,23 +96,23 @@ export class Card {
 
                 const rotationAngle = Math.PI / 6;
                 const rotationSpeed = 0.5;
-
-                function idleAnimation(time) {
+                
+                const idleAnimation = (time) => {
+                    if (!this.skinViewer) return; 
                     const newRotation = rotationAngle * Math.sin(time / 1000 * rotationSpeed);
                     skin.rotation.y = newRotation;
-                    requestAnimationFrame(idleAnimation);
-                }
+                    
+                    this.animationFrameId = requestAnimationFrame(idleAnimation);
+                };
 
-                requestAnimationFrame(idleAnimation);
+                this.animationFrameId = requestAnimationFrame(idleAnimation);
             })
             .catch(err => {
                 console.error("Failed to load skin:", err);
-                console.error("Check the path to the skin image:", this.data.skinImage);
             });
     }
 
-    setupInteractions() {
-    this.container.addEventListener('mousemove', (e) => {
+    handleMouseMove(e) {
         const rect = this.container.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
@@ -108,10 +123,34 @@ export class Card {
         const rotateX = ((y - centerY) / centerY) * -10;
         const rotateY = ((x - centerX) / centerX) * 10;
         this.elements.card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-    });
+    }
 
-    this.container.addEventListener('mouseleave', () => {
+    handleMouseLeave() {
         this.elements.card.style.transform = 'rotateX(0deg) rotateY(0deg)';
-    });
-}
+    }
+
+    setupInteractions() {
+        this.container.addEventListener('mousemove', this.handleMouseMove);
+        this.container.addEventListener('mouseleave', this.handleMouseLeave);
+    }
+    
+    destroy() {
+        console.log("Destroying card instance...");
+
+        this.container.removeEventListener('mousemove', this.handleMouseMove);
+        this.container.removeEventListener('mouseleave', this.handleMouseLeave);
+
+        if (this.animationFrameId) {
+            cancelAnimationFrame(this.animationFrameId);
+        }
+
+        if (this.skinViewer && typeof this.skinViewer.dispose === 'function') {
+            this.skinViewer.dispose();
+        }
+        this.skinViewer = null;
+
+        this.container.innerHTML = '';
+        
+        this.elements = {};
+    }
 }
